90bd1713867f3913e802514747bf13c2e0509e3c
hljxlhqab <qqyb2000@aliyun.com>
2019-05-29 16:51:03 +0800

    branch

---
+++ src/main/java/com/oracle/branch/MyBranch.java
@@ -0,0 +1,13 @@
+package com.oracle.branch;
+
+public class MyBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("我是一个小菜鸟，可以随便改；");
+		
+	}
+
+}
